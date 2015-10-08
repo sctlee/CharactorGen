6e414cac95c0e642b5606d98b7f8ee79d70aaa4d
Secret <haichuang221@163.com>
2015-10-08 17:43:43 +0800

    add login and logout

--- src/example/user/user.go
+++ src/example/user/user.go
@@ -3,7 +3,24 @@ package user
 import (
 	"core/client"
 	"fmt"
+	"strings"
 )
+
+var userList map[*client.Client]*User
+
+type UserAuth string
+
+func (s UserAuth) String() string {
+	switch s {
+	case "lhc":
+		return "lhc"
+	case "hc":
+		return "hc"
+		// ...
+	default:
+		return ""
+	}
+}
 
 func GetUserName(client *client.Client) string {
 	s := userList[client]
@@ -20,4 +37,28 @@ func SetUserName(client *client.Client, paramString string) {
 		Name: name,
 	}
 	client.PutOutgoing(fmt.Sprintf("Hello, %s", name))
+}
+
+func Login(client *client.Client, paramString string) {
+	userInfo := strings.Fields(paramString)
+	if len(userInfo) != 2 {
+		client.PutOutgoing("params number error: Please input correct number of params")
+		return
+	}
+	password := userInfo[1]
+	auth := UserAuth(userInfo[0])
+	if auth != "" && strings.EqualFold(string(auth), password) {
+		userList[client] = &User{
+			Name: userInfo[0],
+		}
+		client.PutOutgoing("Login Success!")
+	}
+}
+
+func Logout(client *client.Client) {
+	if _, ok := userList[client]; ok {
+		client.PutOutgoing("Logout success!")
+	} else {
+		client.PutOutgoing("Please login first!")
+	}
 }
