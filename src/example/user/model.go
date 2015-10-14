6d8f0485bf748e460fb73ff843665b73e5047aa3
Secret <haichuang221@163.com>
2015-10-14 11:14:26 +0800

    refine the code: user module has supported postgresql completely

--- src/example/user/model.go
+++ src/example/user/model.go
@@ -2,9 +2,11 @@ package user
 
 import (
 	"example/db"
+	"log"
 )
 
 type User struct {
+	id       int32
 	Name     string
 	Password string
 }
@@ -13,4 +15,19 @@ func (self *User) Save() error {
 	_, err := db.Pool.Exec("insert into account(name, password) values($1, $2)",
 		self.Name, self.Password)
 	return err
+}
+
+func Exists(name string, password string) (user *User, err error) {
+	// user = nil
+	// err = nil
+	user = &User{}
+
+	row := db.Pool.QueryRow("select id from account where name = $1 and password = $2", name, password)
+	err = row.Scan(&user.id)
+	if err == nil {
+		user.Name = name
+	} else {
+		log.Println(err)
+	}
+	return
 }
