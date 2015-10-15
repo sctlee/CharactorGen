4287b92e7982020c14e26c887a4c5a3f2d3fd7b5
Secret <haichuang221@163.com>
2015-10-15 13:02:31 +0800

    support postgresql completely

--- src/example/chatroom/chatroom.go
+++ src/example/chatroom/chatroom.go
@@ -1,55 +1,88 @@
 package chatroom
 
 import (
-	"example/user"
 	"fmt"
+	"log"
 	"strings"
+
+	"example/user"
 
 	"github.com/sctlee/tcpx"
 	"github.com/sctlee/utils"
 )
 
-var CHATROMMS = []string{"1", "2", "3", "4", "5"}
+var CHATROMMS = []string{}
 
-type ChatRoom struct {
+type Chatroom struct {
+	ct      *Chatrooms
 	clients []*tcpx.Client
 }
 
-var ChatRoomList map[string]*ChatRoom
+var ChatroomList map[string]*Chatroom
 
 var userChatList map[*tcpx.Client]string
 
 func init() {
-	ChatRoomList = make(map[string]*ChatRoom, 10)
+	ChatroomList = make(map[string]*Chatroom, 10)
 	userChatList = make(map[*tcpx.Client]string)
-	for _, k := range CHATROMMS {
-		ChatRoomList[k] = &ChatRoom{}
+}
+
+func initChatrooms() {
+	if len(CHATROMMS) == 0 {
+		log.Println("Init Chatrooms")
+		ctList, err := ListChatrooms()
+		if err != nil {
+			log.Println("Service error")
+		} else {
+			for _, ct := range ctList {
+				ChatroomList[ct.Name] = &Chatroom{
+					ct: ct,
+				}
+				CHATROMMS = append(CHATROMMS, ct.Name)
+			}
+		}
 	}
 }
 
 func List(client *tcpx.Client) {
+	initChatrooms()
 	client.PutOutgoing(fmt.Sprintf("You can choose one chatroom to join:\n%s",
 		strings.Join(CHATROMMS, "\t")))
 }
 
 func Join(client *tcpx.Client, paramString string) {
+	initChatrooms()
 	params := strings.Fields(paramString)
 	if len(params) != 1 {
 		client.PutOutgoing("You can only input one param")
 		return
 	}
 	ctName := params[0]
-	if utils.StringInSlice(ctName, CHATROMMS) {
+	if utils.StringInSlice(ctName, CHATROMMS) != -1 {
+		Exit(client)
 		userChatList[client] = ctName
-		ChatRoomList[ctName].clients = append(ChatRoomList[ctName].clients, client)
+		ChatroomList[ctName].clients = append(ChatroomList[ctName].clients, client)
 		client.PutOutgoing(fmt.Sprintf("you have joined <%s> chatroom", ctName))
+	}
+}
+
+func Exit(client *tcpx.Client) {
+	if k, ok := userChatList[client]; ok {
+		for i, c := range ChatroomList[k].clients {
+			if c == client {
+				ChatroomList[k].clients = append(ChatroomList[k].clients[:i],
+					ChatroomList[k].clients[i+1:]...)
+				client.PutOutgoing(fmt.Sprintf("you have exited <%s> chatroom", k))
+				return
+			}
+		}
 	}
 }
 
 func SendMsg(client *tcpx.Client, paramString string) {
 	msg := paramString
 	if ctName, ok := userChatList[client]; ok {
-		for _, c := range ChatRoomList[ctName].clients {
+		for _, c := range ChatroomList[ctName].clients {
 			c.PutOutgoing(fmt.Sprintf("%s says: %s",
 				user.GetUserName(client),
 				msg),
--- src/example/chatroom/model.go
+++ src/example/chatroom/model.go
@@ -1,8 +1,29 @@
 package chatroom
 
-type ChatRooms struct {
-	id         int32
-	Name       string
-	MaxClients int16
-	Class      string
+import (
+	"example/db"
+)
+
+type Chatrooms struct {
+	id   int32
+	Name string
+	// MaxClients int16
+	Class string
+}
+
+func ListChatrooms() (chatroomList []*Chatrooms, err error) {
+	chatroomList = make([]*Chatrooms, 0)
+
+	rows, _ := db.Pool.Query("select id, name, class from chatrooms")
+
+	for rows.Next() {
+		ct := &Chatrooms{}
+		err := rows.Scan(&ct.id, &ct.Name, &ct.Class)
+		if err == nil {
+			chatroomList = append(chatroomList, ct)
+		}
+	}
+	err = rows.Err()
+	return
+
 }
