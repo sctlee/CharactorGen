16bb0bce5d3f935321cee75e3a3f3e51712c234f
Secret <haichuang221@163.com>
2015-10-30 19:10:25 +0800

    Updated task.go: initial

--- src/features/task/task.go
+++ src/features/task/task.go
@@ -1,34 +1,14 @@
 package task
 
 import (
-	"strings"
-
 	"github.com/sctlee/tcpx"
 )
 
-func Publish(client *tcpx.Client, paramString string) {
-	params := strings.Fields(paramString)
-	if len(params) != 1 {
-		client.PutOutgoing("You can only input one param")
-		return
-	}
-
+func Publish(client *tcpx.Client, params map[string]string) {
 }
 
-func Build(client *tcpx.Client, paramString string) {
-	params := strings.Fields(paramString)
-	if len(params) != 1 {
-		client.PutOutgoing("You can only input one param")
-		return
-	}
-
+func Build(client *tcpx.Client, params map[string]string) {
 }
 
-func Accept(client *tcpx.Client, paramString string) {
-	params := strings.Fields(paramString)
-	if len(params) != 1 {
-		client.PutOutgoing("You can only input one param")
-		return
-	}
-
+func Accept(client *tcpx.Client, params map[string]string) {
 }
